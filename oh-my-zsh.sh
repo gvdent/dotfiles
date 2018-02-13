d8534e831b10899a9665118e158a35d38487fd1b
Gijs van der Ent <gvdent@gmail.com>
2018-02-13 11:19:48 +0100

    Adds some tiny terminal care settings

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,8 @@
 export EDITOR=/usr/bin/vim
 export VISUAL=/usr/bin/vim
 export PATH=${PATH}:/usr/lib/go-1.9/bin/:./node_modules/.bin/
+
+export TTC_APIKEYS=false
+export TTC_WEATHER=Rotterdam
+export TTC_GITBOT=gitlog
+export TTC_REPOS=~/weaver/,~/go/src/weaver-server
