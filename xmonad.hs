9df81ac0eaa9a3e3cdd87eba2094d6c1b68212d2
Gijs van der Ent <gvdent@gmail.com>
2019-05-22 21:18:13 +0200

    Disables xmobar, it caused system hangs

--- xmonad.hs
+++ xmonad.hs
@@ -8,17 +8,7 @@ import XMonad.Layout.NoBorders
 
 --
 -- -- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
---
--- -- Command to launch the bar.
-myBar = "xmobar"
---
--- -- Custom PP, configure it as you like. It determines what is being written
--- to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
---
--- -- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+main = xmonad myConfig
 --
 -- -- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig {
